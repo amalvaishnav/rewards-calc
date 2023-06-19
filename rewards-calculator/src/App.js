64f24bc733c631589ea1d8c377168cec23b1f8b4
Amal Vaishnav <amal.vaishnav96@gmail.com>
2023-06-18 21:40:39 -0400

    Initial commit : Rewards tracker

---
+++ rewards-calculator/src/App.js
@@ -0,0 +1,130 @@
+import { useState, useEffect } from "react";
+import { users } from "./pseudo_database";
+import { calculateRewardsMonthly } from "./rewards_calculator";
+import "./App.css";
+
+/**
+ * A React component which lets the user track all the transaction data
+ */
+function App() {
+  /**
+   * State for the whole live data object
+   */
+  const [userData, setUserData] = useState(users);
+
+  /**
+   * state to add data from the form
+   */
+  const [newAddedData, setNewAddedData] = useState({
+    name: "",
+    amount: 0,
+    date: "2023-06-06",
+  });
+
+  const handleChange = (event) => {
+    setNewAddedData({
+      ...newAddedData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addUserTransactionData(newAddedData);
+  };
+
+  /**
+   * Function to add entry to dummy database from the form
+   * @param {*} newManualyAddedTransaction data coming from the form
+   */
+  const addUserTransactionData = (newManualyAddedTransaction) => {
+    const newUserData = {
+      id: Math.floor(Math.random() * 400000),
+      firstName: newManualyAddedTransaction.name,
+      transactions: [
+        {
+          purchase_date: newManualyAddedTransaction.date,
+          purchase_amount: newManualyAddedTransaction.amount,
+        },
+      ],
+    };
+    setUserData(() => [newUserData, ...userData]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Rewards Tracker</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <h2>Add yourself an entry to test</h2>
+        <br />
+        First Name :
+        <input
+          type="text"
+          value={newAddedData.name}
+          name="name"
+          onChange={handleChange}
+        />
+        <br />
+        Transaction amount :{" "}
+        <input
+          type="number"
+          name="amount"
+          value={newAddedData.amount}
+          onChange={handleChange}
+        />
+        <br />
+        Transaction date :{" "}
+        <input
+          type="date"
+          id="start"
+          name="date"
+          value={newAddedData.date}
+          min="2023-03-15"
+          max="2023-06-15"
+          onChange={handleChange}
+        />
+        <br />
+        <input type="submit" value="Submit" />
+        <br />
+        <br />
+      </form>
+      <table className="rewardTable">
+        <tr>
+          <th>User</th>
+          <th>Transactions</th>
+          <th>Apr rewards</th>
+          <th>May rewards</th>
+          <th>Jun rewards</th>
+          <th>Total Rewards</th>
+        </tr>
+        {userData.map((user) => {
+          return (
+            <tr>
+              <td>{user.firstName}</td>
+              <td>
+                <ol>
+                  {user.transactions.map((item) => [
+                    <li>
+                      Amount : ${item.purchase_amount}, Date :{" "}
+                      {item.purchase_date}
+                    </li>,
+                  ])}
+                </ol>
+              </td>
+              <td>{calculateRewardsMonthly("04", user)}</td>
+              <td>{calculateRewardsMonthly("05", user)}</td>
+              <td>{calculateRewardsMonthly("06", user)}</td>
+              <td>
+                {calculateRewardsMonthly("04", user) +
+                  calculateRewardsMonthly("05", user) +
+                  calculateRewardsMonthly("06", user)}
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ rewards-calculator/src/pseudo_database.js
@@ -0,0 +1,76 @@
+const users = [
+  {
+    id: "1",
+    firstName: "Robin",
+    transactions: [
+      {
+        purchase_date: "2023-05-11",
+        purchase_amount: "120",
+      },
+      {
+        purchase_date: "2023-06-11",
+        purchase_amount: "80",
+      },
+      {
+        purchase_date: "2023-04-11",
+        purchase_amount: "10",
+      },
+      {
+        purchase_date: "2023-06-11",
+        purchase_amount: "220",
+      },
+      {
+        purchase_date: "2023-04-11",
+        purchase_amount: "101",
+      },
+    ],
+  },
+  {
+    id: "2",
+    firstName: "Maria",
+    transactions: [
+      {
+        purchase_date: "2023-05-11",
+        purchase_amount: "55",
+      },
+      {
+        purchase_date: "2023-06-11",
+        purchase_amount: "220",
+      },
+      {
+        purchase_date: "2023-04-11",
+        purchase_amount: "101",
+      },
+    ],
+  },
+  {
+    id: "3",
+    firstName: "Cameroon",
+    transactions: [
+      {
+        purchase_date: "2023-06-11",
+        purchase_amount: "55",
+      },
+      {
+        purchase_date: "2023-04-11",
+        purchase_amount: "45",
+      },
+      {
+        purchase_date: "2023-04-11",
+        purchase_amount: "300",
+      },
+    ],
+  },
+  {
+    id: "4",
+    firstName: "Joe",
+    transactions: [
+      {
+        purchase_date: "2023-05-11",
+        purchase_amount: "100",
+      },
+    ],
+  },
+];
+
+export { users };
